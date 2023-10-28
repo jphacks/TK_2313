d6782c61ac3c9c0f5c131b0b8e29187ab233d8fe
Koichi Hirachi <kouichi.hirachi@gmail.com>
2023-10-28 16:26:46 +0900

    connect voice and gpt

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, UploadFile, File
 from dotenv import load_dotenv
+from fastapi.responses import StreamingResponse
+
+import handler
 
 load_dotenv(verbose=True)
-
 
 app = FastAPI()
 
@@ -15,6 +17,11 @@ async def pong():
 @app.post("/upload/wav")
 async def upload_wav(file: UploadFile = File(...)):
     # save file to ./tmp directory
-    with open(f"./tmp/{file.filename}", "wb") as buffer:
+    with open(f"/tmp/{file.filename}", "wb") as buffer:
         buffer.write(await file.read())
-    return {"filename": file.filename}
+
+    stream = handler.handle_voice_driven(f"/tmp/{file.filename}")
+    # save to /tmp
+
+
+
