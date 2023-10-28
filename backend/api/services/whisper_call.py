05825a719454a9f5e1abaff8c308fa43803d39a0
Koichi Hirachi <kouichi.hirachi@gmail.com>
2023-10-28 16:26:46 +0900

    connect voice and gpt

--- backend/api/services/whisper_call.py
+++ backend/api/services/whisper_call.py
@@ -1,5 +1,6 @@
 import openai
 
-def whisper_transcription(audio_file):
+
+def whisper_transcription(audio_file: bytes):
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
     return transcript['text']
