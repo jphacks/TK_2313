0e894fdeb9af8acf249556c7a29c8093eee8c3bc
chroma7p <nahco3.hcl@gmail.com>
2023-11-04 16:44:44 +0900

    WIP

--- backend/api/services/voicevox_call.py
+++ backend/api/services/voicevox_call.py
@@ -1,31 +1,81 @@
 import os
 import requests
 import json
+from typing import List
+from dotenv import load_dotenv
+
+load_dotenv(".env.default")
+
+HOST = os.environ.get('VOICEVOX_ENGINE_HOST')
+PORT = int(os.environ.get('VOICEVOX_ENGINE_PORT'))
+
+
+
+class Character:
+    def __init__(self,name:str,uuid:str,styles:dict[str,int]):
+        self.name:str = name
+        self.uuid:str = uuid
+        self.styles:dict[str,int] = styles
+
+    def get_params(self,text:str,style_id:str):
+        return {
+            'text': text,
+            'style': style_id
+        }
+
+    def get_query(self,text:str,style_id:str):
+        return requests.post(
+            f'http://{HOST}:{PORT}/characters',
+            headers={"Content-Type": "application/json"},
+            data=json.dumps(self.get_params(text,style_id))
+        )
+        
+    def make_voice(self,text:str,style:str|int):
+        if type(style) == str:
+            style = self.get_style_id(style)
+        query = self.get_query(text,style)
+        synthesis = requests.post(
+            f'http://{HOST}:{PORT}/synthesis',
+            headers={"Content-Type": "application/json"},
+            data=json.dumps(query.json())
+        )
+        return synthesis.content
+
+
+    def get_styles_name(self):
+        return list(self.styles.keys())
+    
+    def get_style_id(self,style_name:str):
+        if style_name not in self.styles:
+            raise Exception(f'{style_name} is not in {self.name}')
+        return self.styles[style_name]
+
+    def __str__(self) -> str:
+        return f'{self.name}:{" ".join(self.styles.keys())}'
+    
+def make_speaker_list():
+    with open('api/services/speakers.json','r') as f:
+        speaker_list = json.load(f)
+    for speaker in speaker_list:
+        speaker['uuid'] = speaker['speaker_uuid']
+        speaker['style'] = dict()
+        for style in speaker['styles']:
+            speaker['style'][style['name']] = style['id']
+        #print(speaker["styles"])
+        
+    return {speaker['name']:Character(speaker['name'],speaker['uuid'],speaker['style']) for speaker in speaker_list}
+    
+speakers=make_speaker_list()
+
+
 
 
 def vvox_test(text):
-    host = os.environ.get('VOICEVOX_ENGINE_HOST')
-    port = int(os.environ.get('VOICEVOX_ENGINE_PORT'))
+    speaker=speakers['櫻歌ミコ']
+    return speaker.make_voice(text,'ノーマル')
 
-    # 音声化する文言と話者を指定(3で標準ずんだもんになる)
-    params = (
-        ('text', text),
-        # ('speaker', 8),
-        ('speaker', 43)
-    )
 
-    # 音声合成用のクエリ作成
-    query = requests.post(
-        f'http://{host}:{port}/audio_query',
-        params=params
-    )
-
-    # 音声合成を実施
-    synthesis = requests.post(
-        f'http://{host}:{port}/synthesis',
-        headers={"Content-Type": "application/json"},
-        params=params,
-        data=json.dumps(query.json())
-    )
-
-    return synthesis.content
+if __name__ == "__main__":
+    for s in speakers:
+        print(s)   
+    vvox_test('こんにちは')
